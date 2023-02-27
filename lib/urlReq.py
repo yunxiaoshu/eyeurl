9bbfe8942ebaca382c5b7ff9ec4d136abc16550f
yunxiaoshu <932432713@qq.com>
2023-02-27 10:32:55 +0800

    更新

--- lib/urlReq.py
+++ lib/urlReq.py
@@ -13,23 +13,3 @@ class Request:
         resp = self.s.post(url, headers=header,data=data,verify=verify, timeout=timeout,allow_redirects=allow_redirects)
         self.s.close()
         return resp
-
-
-# from webHack import urlReq
-# import time
-# if __name__ == '__main__':
-#     # url='https://sts.didiglobal.com/sg1/api/free/signal-upm-service/pc_login'
-#     url='https://epassport.diditaxi.com.cn'
-#     header={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36","Connection":"close"}
-#     urlpaste= urlReq.Request()
-#     with open('C:\\Users\\93243\\Desktop\\url.txt','r') as f:
-#         while True:
-#             line = f.readline()
-#             if line:
-#                 line=line.replace('\n','')
-#                 resp=urlpaste.req_post(url+line,header)
-#                 print("{0}\t{1}".format(url,resp.text))
-#                 time.sleep(1.5)
-#             else:
-#                 break
-#         f.close()
